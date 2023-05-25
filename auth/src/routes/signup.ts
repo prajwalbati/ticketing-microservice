978820b1f2fdda03f87230732617b160090e303e
Prajwal Bati <prajwalbati@gmail.com>
2023-05-25 16:07:13 -0400

    user model

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
 
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({min: 4, max: 20})
         .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
@@ -22,10 +23,14 @@ router.post('/api/users/signup', [
 
     const { email, password } = req.body;
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        throw new BadRequestError('Email in use');
+    }
+    const user = await User.build({ email, password });
+    await user.save();
 
-    res.send({});
+    return res.status(201).send(user);
 });
 
 export { router as signupRouter };
