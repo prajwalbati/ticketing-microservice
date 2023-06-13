6d0b8bfe234d4bda8d85880acf997e5fe409348f
Prajwal Bati <prajwalbati@gmail.com>
2023-06-13 00:22:21 -0400

    authentication middlewares

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,18 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    if (!req.session?.jwt) {
-        return res.send({ currentUser: null });
-    }
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        return res.send({currentUser: payload});
-    } catch (error) {
-        return res.send({ currentUser: null });
-    }
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+    return res.send({currentUser: req.currentUser});
 });
 
 export { router as currentUserRouter };
