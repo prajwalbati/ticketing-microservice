7bcb82361c3e97d76711c2dd8ea68a3b592002fd
Prajwal Bati <prajwalbati@gmail.com>
2023-06-20 23:09:23 -0400

    add global signin test setup

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,21 +2,21 @@ import request from "supertest";
 import { app } from "../../app";
 
 it('responds with details about the current user', async() => {
-    let signUpResponse = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201);
-
-    let cookieDetails = signUpResponse.get('Set-Cookie');
+    let cookie = await global.signin();
 
     let response = await request(app)
         .get('/api/users/currentuser')
-        .set('Cookie', cookieDetails)
+        .set('Cookie', cookie)
         .send()
         .expect(200);
 
     expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async() => {
+    let response = await request(app)
+        .get('/api/users/currentuser')
+        .send();
+
+    expect(response.body.currentUser).toEqual(undefined);
 });
