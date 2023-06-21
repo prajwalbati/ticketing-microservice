0e5c074eb66fa35d3a768273d600d8c2254c8080
Prajwal Bati <prajwalbati@gmail.com>
2023-06-20 22:41:24 -0400

    sign in testcases

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it('fails when a email that does not exit is supplied', async () => {
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('fails when incorrect password is supplied', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password1'
+        })
+        .expect(400);
+});
+
+it('responds with a cookie when given valid credentials', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    let response = await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+});
