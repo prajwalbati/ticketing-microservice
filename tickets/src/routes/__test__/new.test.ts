a0a8f4b95230a2e6cb7e01f45036a0bb46233429
Prajwal Bati <prajwalbati@gmail.com>
2023-08-04 16:28:11 -0400

    create ticket feature and tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async() => {
     const response = await request(app)
@@ -19,21 +20,69 @@ it('can only be accessed if the user is signed in', async() => {
 });
 
 it('returns a status other than 401 if the user is signed in', async() => {
+    let cookie = global.signin();
     const response = await request(app)
         .post('/api/tickets')
+        .set('Cookie', cookie)
         .send({});
 
     expect(response.status).not.toEqual(401);
 });
 
 it('returns an error if an invalid title is provided', async() => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            price: 10
+        })
+        .expect(400);
 
 });
 
 it('returns an error if an invalid price is provided', async() => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'New ticket',
+        })
+        .expect(400);
 
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'New ticket',
+            price: -10
+        })
+        .expect(400);
 });
 
 it('creates a ticket with valid inputs', async() => {
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
 
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'New ticket',
+            price: 10
+        })
+        .expect(201);
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].price).toEqual(10);
+    expect(tickets[0].title).toEqual('New ticket');
 });
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,10 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
+
 import { app } from '../app';
 
+
 declare global {
-    var signin: () => Promise<string[]>;
+    var signin: () => string[];
 }
 
 let mongo: any;
@@ -33,16 +36,25 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-global.signin = async () => {
-    const email = 'test@test.com';
-    const password = 'password';
+global.signin = () => {
+    // build a JWT payload. { id, email }
+    const payload = {
+        id: '12',
+        email: 'test@test.com'
+    };
 
-    const response = await request(app)
-        .post('/api/users/signup')
-        .send({ email, password })
-        .expect(201);
+    // create the JWT!
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-    const cookie = response.get('Set-Cookie');
+    // Build session object. { jwt: MY_JWT}
+    const session = { jwt: token };
 
-    return cookie;
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session);
+
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+
+    // return a string that is the cookie with encoded data
+    return [`session=${base64}`];
 };
