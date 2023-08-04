5e8cd91fc67b96901748045a979a8a723747768b
Prajwal Bati <prajwalbati@gmail.com>
2023-08-04 01:20:33 -0400

    add tests for tickets api

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,9 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@satik-tickets/common';
 
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +16,9 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 );
+
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.get('*', () => {
     throw new NotFoundError();
