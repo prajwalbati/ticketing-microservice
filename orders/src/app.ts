74d18d06b2e3b2473e0d47c4b2956801cfdb905f
Prajwal Bati <prajwalbati@gmail.com>
2023-11-07 14:38:56 -0500

    add order service implementation

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@satik-tickets/common';
+
+import { createOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+
+app.use(currentUser);
+
+app.use(createOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
+
+app.get('*', () => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
