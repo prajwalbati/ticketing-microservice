191afaca78acf2b7caa1581f87616a60c47e1ab0
Prajwal Bati <prajwalbati@gmail.com>
2023-11-23 19:40:21 -0500

    test commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+    console.log("Starting up application");
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
